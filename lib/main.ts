2764248c86a191b326eca32bef0f5d8df282cc65
Paul Rostorp <paul.rostorp@gmail.com>
2022-06-21 13:26:32 +0200

    basic

---
+++ lib/main.ts
@@ -0,0 +1,16 @@
+import core from '@actions/core'
+import github from '@actions/github'
+import { getVersion } from './calver'
+
+try {
+  const prev = core.getInput('prev')
+  const microPrefix = core.getInput('microPrefix')
+  const version = getVersion(prev, microPrefix)
+
+  core.setOutput('version', version)
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+} catch (error:any) {
+  core.setFailed(error.message)
+}
